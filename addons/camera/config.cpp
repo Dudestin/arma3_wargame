19ed6bd2fda3ac88d24172838a118ad00e5795e7
Dudestin <splashakaponear@gmail.com>
2020-03-03 15:55:25 +0900

    add createDeployDisp

---
+++ addons/camera/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches {
+  class ADDON {
+    name = "Arma3-Wargame";
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = 0.1;
+    requiredAddons[] = {};
+    authors[] = {"Dudestin"};
+  };
+};
