109df3276d80828d3c5e64a82fa778a2eb3e1c06
Dudestin <splashkakaponear@gmail.com>
2020-03-09 23:46:45 +0900

    edit main/script_mod.hpp

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -8,4 +8,11 @@
 #define VERSION_AR  MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.96
+#define REQUIRED_VERSION 1.92
+#define REQUIRED_CBA_VERSION {3,10,1}
+
+#ifdef COMPONENT_BEAUTIFIED
+	#define COMPONENT_NAME QUOTE(ZEN - COMPONENT_BEAUTIFIED)
+#else
+	#define COMPONENT_NAME QUOTE(ZEN - COMPONENT)
+#endif
